bce8751d87a7988eb69aaf583d30865963f16524
ch3lidze <ch3lidze@yahoo.com>
2022-07-18 18:00:35 +0400

    this is grid gallery for my friends this was my old css project and I updated a bit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const colors =['#e68383','#c5dc52', '#ce34b9', '#ebe95c', '#57eb93' ]
-    const section = document.querySelector('section')
+const colors =['#e68383','#c5dc52', '#ce34b9', '#ebe95c', '#57eb93' ];
     function changeBg(){
     const bg = colors[Math.floor(Math.random() * colors.length -1)];
-    section.style.backgroundColor = bg;
+    document.body.style.backgroundColor = bg;
     }
-    setInterval(changeBg, 4000)
+    setInterval(changeBg, 6000);
